8c8b4b29851df5dea9f035cf98cfa95e68305d5c
Rin Kaoki <lemi013794@gmail.com>
2021-06-28 09:55:13 +0700

    Готово

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -27,7 +27,31 @@ namespace Exam
 
         private void Button_Click(object sender, RoutedEventArgs e)
         {
-            
+            if (tb_count.Text == "")
+                otvet.Content = ""; 
+            else
+            {
+                string[] str = tb_count.Text.Select(i => i.ToString()).ToArray();
+                int cod = Convert.ToInt32(str[str.Count() - 2]) * 10 + Convert.ToInt32(str[str.Count() - 1]);
+                string msg = "";
+                if (str.Count() == 3)
+                {
+                    if (cod < 10)
+                        msg = "Диаметр подшипника равен = " + Convert.ToString(cod);
+                }
+                else if (str.Count() == 4)
+                {
+                    if (cod == 0)
+                        msg = "Диаметр подшипника равен = 10";
+                    else if (cod == 1)
+                        msg = "Диаметр подшипника равен = 12";
+                    else if (cod == 3)
+                        msg = "Диаметр подшипника равен = 17";
+                    else if (cod >= 4)
+                        msg = "Диаметр подшипника равен = " + Convert.ToString(cod * 5);
+                }
+                otvet.Content = msg;
+            }
         }
     }
 }
