97ff6dd5dbaad40394872fbbe04dcd3763e0b9d6
Rin Kaoki <lemi013794@gmail.com>
2021-06-28 10:10:27 +0700

    Небольший космитические доработки и теперь точно готово

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -27,7 +27,7 @@ namespace Exam
 
         private void Button_Click(object sender, RoutedEventArgs e)
         {
-            if (tb_count.Text == "")
+            if (tb_count.Text == "" || tb_count.Text.Count() < 3 || tb_count.Text.Count() > 4)
                 otvet.Content = ""; 
             else
             {
@@ -37,18 +37,20 @@ namespace Exam
                 if (str.Count() == 3)
                 {
                     if (cod < 10)
-                        msg = "Диаметр подшипника равен = " + Convert.ToString(cod);
+                        msg = "Внутрений диаметр подшипника равен = " + Convert.ToString(cod) + "мм \n(Внутрений радиус = " + Convert.ToString(cod/2) + "мм)";
                 }
                 else if (str.Count() == 4)
                 {
                     if (cod == 0)
-                        msg = "Диаметр подшипника равен = 10";
+                        msg = "Внутрений диаметр подшипника равен = 10мм \n(Внутрений радиус = 5мм)";
                     else if (cod == 1)
-                        msg = "Диаметр подшипника равен = 12";
+                        msg = "Внутрений диаметр подшипника равен = 12мм \n(Внутрений радиус = 6мм)";
+                    else if (cod == 2)
+                        msg = "Внутрений диаметр подшипника равен = 15мм \n(Внутрений радиус = 7.5мм)";
                     else if (cod == 3)
-                        msg = "Диаметр подшипника равен = 17";
+                        msg = "Внутрений диаметр подшипника равен = 17 \n(Внутрений радиус = 8.5мм)";
                     else if (cod >= 4)
-                        msg = "Диаметр подшипника равен = " + Convert.ToString(cod * 5);
+                        msg = "Внутрений диаметр подшипника равен = " + Convert.ToString(cod * 5) + "мм \n(Внутрений радиус = " + Convert.ToString(cod*5 / 2) + "мм)";
                 }
                 otvet.Content = msg;
             }
